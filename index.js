79bbfc14a08e8840c0feb4c6ed4a1029e109cbd2
AH-sunny <sunnycse03@gmail.com>
2024-11-15 11:31:33 +0600

    jwt implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config()
 const app = express()
@@ -14,33 +15,47 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  const dbConnect = async()=>{
-    try{
-        client.connect();
-        console.log("mongodb connected successfully");
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+const dbConnect = async () => {
+  try {
+    client.connect();
+    console.log("mongodb connected successfully");
+
+    //jwt
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body
+      console.log(userEmail)
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, {
+        expiresIn: '10d'
+      })
+      res.send({ token })
+    })
 
 
-        
+
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
 
 
-    }catch(error){
-        console.log(error.name, error.message);
-    }
-
-
-  };
-  dbConnect()
+};
+dbConnect()
 
 //api
-app.get("/", (req,res)=>{
-    res.send("server is running");
+app.get("/", (req, res) => {
+  res.send("server is running");
+
+
 })
-app.listen(port,()=>{
-    console.log(`server is running on port, ${port}`);
+
+
+
+
+app.listen(port, () => {
+  console.log(`server is running on port, ${port}`);
 })
